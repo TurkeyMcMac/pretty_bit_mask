fb732916ac25b1e6f112145a003ec01c9d6c189f
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-08-28 07:49:18 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+#![feature(i128_type)]
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn it_works() {
+        let mut m: u32 = 0;
+        
+        m.flip(128);
+        m.mask(1|4|8|64);
+        m.flip(128);
+        m.unmask(8);
+        
+        assert!(m.masked(4|64));
+        assert!(!m.masked(8|128));
+    }
+}
+
+use std::ops::{
+    BitOrAssign,
+    BitXorAssign,
+    BitAndAssign,
+    Not,
+    BitAnd,
+};
+
+pub trait BitMaskable<B: Copy> {
+    fn mask(&mut self, mask: B);
+    
+    fn flip(&mut self, mask: B);
+    
+    fn unmask(&mut self, mask: B);
+    
+    fn masked(self, mask: B) -> bool;
+}
+
+impl <B> BitMaskable<B> for B
+    where B: Copy +
+             BitOrAssign +
+             BitXorAssign +
+             BitAndAssign +
+             Not<Output = B> +
+             BitAnd<B, Output = B> +
+             Eq,
+{
+    fn mask(&mut self, mask: B) {
+        *self |= mask;
+    }
+    
+    fn flip(&mut self, mask: B) {
+        *self ^= mask;
+    }
+    
+    fn unmask(&mut self, mask: B) {
+        *self &= !mask;
+    }
+    
+    fn masked(self, mask: B) -> bool {
+        self & mask == mask
+    }
+}
